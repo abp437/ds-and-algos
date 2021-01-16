b1a9d9e959c4aef31517a3d062d1fc3e5e2d71f4
abp437 <abp437@gmail.com>
2021-01-16 23:08:54 +0530

    Add Valid Anagram Code

--- javascript/problem-solving-patterns/validAnagram.js
+++ javascript/problem-solving-patterns/validAnagram.js
@@ -16,11 +16,70 @@ Time Complexity - O(n)
 */
 
 // Naive Way:
-function validAnagram(testString, anagram) {
+function akshayValidAnagram(testString, anagram) {
+  if (!(typeof testString === 'string' && typeof anagram === 'string')) {
+    throw new Error('Expected to pass strings in this function!');
+  }
 
+  const anagramLength = anagram.length;
+  const testStringLength = testString.length;
+
+  if (testStringLength !== anagramLength) {
+    return false;
+  }
+
+  let result = true;
+
+  const tesStringObj = {};
+  for (let i = 0; i < testStringLength; i++) {
+    const element = testString[i];
+    tesStringObj[element] = (tesStringObj[element] || 0) + 1;
+  }
+
+  const anagramObj = {};
+  for (let i = 0; i < anagramLength; i++) {
+    const element = anagram[i];
+    anagramObj[element] = (anagramObj[element] || 0) + 1;
+  }
+
+  for (const key in tesStringObj) {
+    if (Object.hasOwnProperty.call(tesStringObj, key)) {
+      if (tesStringObj[key] !== anagramObj[key]) {
+        result = false;
+        break;
+      }
+    }
+  }
+
+  return result;
 }
 
 // Instructor's Valid Anagram Frequency Counter Way:
+function validAnagram(first, second) {
+  if (first.length !== second.length) {
+    return false;
+  }
+
+  const lookup = {};
+
+  for (let i = 0; i < first.length; i++) {
+    let letter = first[i];
+    // if letter exists, increment, otherwise set to 1
+    lookup[letter] ? (lookup[letter] += 1) : (lookup[letter] = 1);
+  }
+
+  for (let i = 0; i < second.length; i++) {
+    let letter = second[i];
+    // can't find letter or letter is zero then it's not an anagram
+    if (!lookup[letter]) {
+      return false;
+    } else {
+      lookup[letter] -= 1;
+    }
+  }
+
+  return true;
+}
 
 console.log(validAnagram('', ''));
 console.log(validAnagram('aaz', 'zza'));
