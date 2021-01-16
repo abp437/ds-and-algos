41dd1b4dbda49e72c66d9fad63cc40307757a9ac
abp437 <abp437@gmail.com>
2021-01-16 22:16:34 +0530

    Naive Solutions for Frequency counter pattern

--- javascript/problem-solving-patterns/frequencyCounter.js
+++ javascript/problem-solving-patterns/frequencyCounter.js
@@ -7,25 +7,70 @@ Examples:
 same([1, 2, 3], [4, 1, 9]); => true
 same([1, 2, 3], [1, 9]); => false
 same([1, 2, 1], [4, 4, 1]); => false (must be same frequency)
+same([1, 2, 1], [1, 4, 1]); => true
 */
 
-// Naive way:
-
+// Akshay's Naive way:
 function same(source, dest) {
   if (!(Array.isArray(source) && Array.isArray(dest))) {
     throw new Error('Expected to pass arrays in this function!');
   }
 
-  source.forEach((element) => {
+  if (source.length !== dest.length) {
+    return false;
+  }
 
+  let result = true;
+
+  const sourceObj = {};
+  source.forEach((element) => {
+    sourceObj[element] = sourceObj[element] + 1 || 1;
   });
 
-  return true
+  const destObj = {};
+  dest.forEach((element) => {
+    destObj[element] = destObj[element] + 1 || 1;
+  });
+
+  for (let i = 0; i < source.length; i++) {
+    const element = source[i];
+    if (sourceObj[element] !== destObj[element ** 2]) {
+      result = false;
+      break;
+    }
+  }
+
+  return result;
+}
+
+// Shirish's Way:
+function shirishSame(source, dest) {
+  if (!(Array.isArray(source) && Array.isArray(dest))) {
+    throw new Error('Expected to pass arrays in this function!');
+  }
+
+  if (source.length !== dest.length) {
+    return false;
+  }
+
+  let result = true;
+
+  source.sort();
+  dest.sort();
+
+  for (let i = 0; i < source.length; i++) {
+    if (dest[i] !== source[i] ** 2) {
+      result = false;
+      break;
+    }
+  }
+
+  return result;
 }
 
 console.log(same([1, 2, 3], [4, 1, 9]));
 console.log(same([1, 2, 3], [1, 9]));
 console.log(same([1, 2, 1], [4, 4, 1]));
-
+console.log(same([1, 2, 1], [1, 1, 4]));
 
 // Frequency Counter Way:
