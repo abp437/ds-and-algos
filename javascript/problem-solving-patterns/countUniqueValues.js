479cb4dda502ed6b9ebdc714526d873e888b9a87
abp437 <abp437@gmail.com>
2021-01-17 20:13:23 +0530

    Add count unique values naive solution

---
+++ javascript/problem-solving-patterns/countUniqueValues.js
@@ -0,0 +1,47 @@
+/* Problem Statement:
+Implement a function called 'countUniqueValues',
+which accepts a sorted array and counts the unique values in the array.
+There can be negative numbers in the array, but it will always be sorted.
+
+Examples:
+countUniqueValues([1, 1, 1, 1, 1, 2]) => 2
+countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]) => 7
+countUniqueValues([]) => 0
+countUniqueValues([-2, -1, -1, 0, 1])  => 4
+*/
+
+// Naive way:
+function countUniqueValues(sortedArray) {
+  if (!Array.isArray(sortedArray)) {
+    throw new Error('Expected to pass an array to this function!');
+  }
+
+  let left = 0;
+  let right = sortedArray.length - 1;
+
+  const uniqueCount = {};
+  while (left < right) {
+    const leftValue = sortedArray[left];
+    const rightValue = sortedArray[right];
+
+    if (!uniqueCount[leftValue]) {
+      uniqueCount[leftValue] = true;
+    }
+    if (!uniqueCount[rightValue]) {
+      uniqueCount[rightValue] = true;
+    }
+
+    left++;
+    right--;
+  }
+
+  return Object.keys(uniqueCount).length;
+}
+
+// Instructor's Count Unique Values Way:
+
+
+console.log(countUniqueValues([1, 1, 1, 1, 1, 2]));
+console.log(countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]));
+console.log(countUniqueValues([]));
+console.log(countUniqueValues([-2, -1, -1, 0, 1]));
